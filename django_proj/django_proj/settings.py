794ca3931be9fd49edd0d2bc030cd9ee63809cbb
1ceHat <artemdudorov2015@gmail.com>
2024-07-26 22:29:43 +0500

    migrate to postgres

--- django_proj/django_proj/settings.py
+++ django_proj/django_proj/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'django_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db',
+        'USER': 'postgres',
+        'PASSWORD': '123234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
